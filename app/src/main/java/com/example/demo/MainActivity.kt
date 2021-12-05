bb9d6ce5bf19db6e1958f320e5a0e25e29137724
parsaki <parsa.kiyani.1380.iran@gmail.com>
2021-12-05 14:25:48 +0330

    full

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,20 +2,28 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import android.util.Log
+import androidx.activity.viewModels
+import com.example.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+    private val model : MyViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        model.counter.observe(this, {
+            binding.textView.text = it.toString()
+            Log.d("TAG", "onCreate: clicked")
+        })
+
+        binding.button.setOnClickListener {
+            model.increment()
+        }
 
     }
 }
--- app/src/main/java/com/example/demo/MyViewModel.kt
+++ app/src/main/java/com/example/demo/MyViewModel.kt
@@ -3,7 +3,12 @@ package com.example.demo
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class MyViewModel : ViewModel() {
 
+    var counter = MutableLiveData<Int>(0)
 
+    fun increment() {
+        counter.value = counter.value?.plus(1)
+    }
 }
