531230066690af19208c1d016d6cee12c10372b7
parsaki <parsa.kiyani.1380.iran@gmail.com>
2021-12-21 02:07:07 +0330

    update

--- design279vclass/app/src/main/java/com/example/design27_9vclass/ContactsAdapter.kt
+++ design279vclass/app/src/main/java/com/example/design27_9vclass/ContactsAdapter.kt
@@ -1,26 +1,32 @@
 package com.example.design27_9vclass
 
+import android.content.res.Resources
+import android.inputmethodservice.ExtractEditText
 import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.text.set
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactsAdapter(private val mContacts: Array<Contact>) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+class ContactsAdapter(private var mContacts: ArrayList<Items>) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
     public fun getItemviewType(position: Int):Int{
-        if(position == mContacts.size) return R.layout.butrew else  return R.layout.costum_recicle_viwe
+        if(position == mContacts.size-1&&position !=0) return R.layout.butrew else  return R.layout.costum_recicle_viwe
     }
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Your holder should contain and initialize a member variable
         // for any view that will be set as you render a row
-        val edittext1 = view.findViewById<EditText>(R.id.textrw1)
-        val edittext2 = view.findViewById<EditText>(R.id.textrw2)
+        val edittext1 = view.findViewById<AppCompatEditText>(R.id.textrw1)
+        val edittext2 = view.findViewById<AppCompatEditText>(R.id.textrw2)
         val edittext3 = view.findViewById<EditText>(R.id.textrw3)
         val edittext4 = view.findViewById<EditText>(R.id.textrw4)
-       // val addbut = itemView.findViewById<AppCompatButton>(R.id.but_add)
+        val addbut = itemView.findViewById<AppCompatButton>(R.id.but_add)
     }
     override fun onCreateViewHolder(viewCroup: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
         val view = LayoutInflater.from(viewCroup.context)
@@ -31,10 +37,22 @@ class ContactsAdapter(private val mContacts: Array<Contact>) : RecyclerView.Adap
     // Involves populating data into the item through holder
     override fun onBindViewHolder(viewHolder: ContactsAdapter.ViewHolder, position: Int) {
         // Get the data model based on position
-        viewHolder.edittext1.text = mContacts[position].text.toEditable()
+      //  viewHolder.edittext1.setText("g")
+        //viewHolder.edittext1.setText( mContacts[position].text.toEditable())
+        //viewHolder.edittext2.setText( mContacts[position].text2.toEditable())
+        //viewHolder.edittext3.setText( mContacts[position].text3.toEditable())
+        //viewHolder.edittext4.setText(mContacts[position].text4.toEditable())
        // text2.setText(contact.text2)
        // text3.setText(contact.text3)
        // text4.setText(contact.text4)
+
+        viewHolder.addbut.visibility = View.INVISIBLE
+        if(position == mContacts.size-1)  viewHolder.addbut.visibility = View.VISIBLE
+        viewHolder.addbut.setOnClickListener {
+            contacts.add(Items("","","","654"))
+            this.mContacts= contacts
+            this.notifyDataSetChanged()
+        }
         // Set item views based on your views and data model
     }
     // Returns the total count of items in the list
@@ -43,14 +61,4 @@ class ContactsAdapter(private val mContacts: Array<Contact>) : RecyclerView.Adap
 
 }
 
-class Contact(val text: String,val text2: String,val text3: String,val text4: String) {
-    companion object {
-        private var lastContactId = 0
-        fun createContactsList(etext: String,etext2: String,etext3: String,etext4: String): ArrayList<Contact> {
-            val contacts = ArrayList<Contact>()
-                contacts.add(Contact(etext,etext2,etext3,etext4))
-            return contacts
-        }
-    }
-}
-
+data class Items (val text:String, val text2:String, val text3:String, val text4:String)
