9ee72219b30dedc972bb216f2372129a890c51fe
parsaki <parsa.kiyani.1380.iran@gmail.com>
2021-12-21 15:10:08 +0330

    update

--- design279vclass/app/src/main/java/com/example/design27_9vclass/ContactsAdapter.kt
+++ design279vclass/app/src/main/java/com/example/design27_9vclass/ContactsAdapter.kt
@@ -22,10 +22,10 @@ class ContactsAdapter(private var mContacts: ArrayList<Items>) : RecyclerView.Ad
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Your holder should contain and initialize a member variable
         // for any view that will be set as you render a row
-        val edittext1 = view.findViewById<AppCompatEditText>(R.id.textrw1)
-        val edittext2 = view.findViewById<AppCompatEditText>(R.id.textrw2)
-        val edittext3 = view.findViewById<EditText>(R.id.textrw3)
-        val edittext4 = view.findViewById<EditText>(R.id.textrw4)
+        var edittext1 = view.findViewById<AppCompatEditText>(R.id.textrw1)
+        var edittext2 = view.findViewById<AppCompatEditText>(R.id.textrw2)
+        var edittext3 = view.findViewById<EditText>(R.id.textrw3)
+        var edittext4 = view.findViewById<EditText>(R.id.textrw4)
         val addbut = itemView.findViewById<AppCompatButton>(R.id.but_add)
     }
     override fun onCreateViewHolder(viewCroup: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
@@ -37,19 +37,11 @@ class ContactsAdapter(private var mContacts: ArrayList<Items>) : RecyclerView.Ad
     // Involves populating data into the item through holder
     override fun onBindViewHolder(viewHolder: ContactsAdapter.ViewHolder, position: Int) {
         // Get the data model based on position
-      //  viewHolder.edittext1.setText("g")
-        //viewHolder.edittext1.setText( mContacts[position].text.toEditable())
-        //viewHolder.edittext2.setText( mContacts[position].text2.toEditable())
-        //viewHolder.edittext3.setText( mContacts[position].text3.toEditable())
-        //viewHolder.edittext4.setText(mContacts[position].text4.toEditable())
-       // text2.setText(contact.text2)
-       // text3.setText(contact.text3)
-       // text4.setText(contact.text4)
 
         viewHolder.addbut.visibility = View.INVISIBLE
         if(position == mContacts.size-1)  viewHolder.addbut.visibility = View.VISIBLE
         viewHolder.addbut.setOnClickListener {
-            contacts.add(Items("","","","654"))
+            contacts.add(Items("","","",""))
             this.mContacts= contacts
             this.notifyDataSetChanged()
         }
