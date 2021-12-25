5caf1cf7089988e02beff10cc875e3549da3f91f
parsaki <parsa.kiyani.1380.iran@gmail.com>
2021-12-25 16:56:15 +0330

    a

---
+++ TabView/app/src/main/java/com/example/tabview/sec_flag.kt
@@ -0,0 +1,21 @@
+package com.example.tabview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class sec_flag:Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.sec_flag,container,false)
+    }
+}
