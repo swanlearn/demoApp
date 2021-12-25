01b5d611df03f3e7b170db2dfa5d15caffbd9ffb
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2021-12-25 12:56:01 +0330

    Test

---
+++ Test/src/main/java/com/tispunshahryar960103/test/MyClass.kt
@@ -0,0 +1,86 @@
+package com.tispunshahryar960103.test
+
+fun main() {
+
+    //fibonacci(10)
+    println( factorial(5))
+
+
+}
+
+
+/**
+ * Prime Nimbers
+ */
+fun printPrimeNumbers(num: Int) {
+
+    val numList= mutableListOf<Int>()
+
+    if (num <= 1) {
+        println("Please Enter a number greater than 1")
+    } else {
+        for (i in 2 .. num) {
+           for (j in 2 until i){
+               if (i % j != 0){
+                   numList.add(i)
+               }
+           }
+        }
+    }
+
+    println("the count of prime numbers in 1 until $num is : ${numList.size} ")
+}
+
+
+/**
+ * Fibonacci Series
+ */
+
+fun fibonacci(num: Int) {
+
+    var a = 0
+    var b = 1
+    var c: Int
+    println(a)
+    println(b)
+
+    for (i in 1..num) {
+        c = a + b
+        println(c)
+        a = b
+        b = c
+    }
+
+}
+
+/**
+ * Factorial
+ */
+
+fun factorial(num: Long): Long {
+    var facto: Long = num
+
+    when (num) {
+        0L -> println("The factorial of $num is : 1")
+        1L -> println("Please enter a number bigger than ONE!")
+        else -> {
+
+
+            for (i in 2 until num) {
+
+
+                facto *= i
+
+            }
+
+
+        }
+    }
+
+    return facto
+
+
+}
+
+
+
