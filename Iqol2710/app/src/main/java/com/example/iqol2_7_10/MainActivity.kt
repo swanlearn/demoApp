65cd107aac195af3da5abbdb5063e8c3609e663f
parsaki <parsa.kiyani.1380.iran@gmail.com>
2021-12-29 14:36:43 +0330

    update

--- Iqol2710/app/src/main/java/com/example/iqol2_7_10/MainActivity.kt
+++ Iqol2710/app/src/main/java/com/example/iqol2_7_10/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.iqol2_7_10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.frag_view,HomeFrag()).commit()
+        val nav = findViewById<NavigationView>(R.id.nav_view)
+        val nav_view = nav.getHeaderView(0)
+        val but1 = nav_view.findViewById<AppCompatButton>(R.id.profilebtn)
+        val but2 = nav_view.findViewById<AppCompatButton>(R.id.reportbtn)
+        val but3 = nav_view.findViewById<AppCompatButton>(R.id.secbtn)
+        val but4 = nav_view.findViewById<AppCompatButton>(R.id.setbtn)
+        but1.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frag_view,changeprofile_frag()).commit()
+        }
+        but2.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frag_view,ReportFrag()).commit()
+        }
+        but4.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frag_view,welcomeFrag()).commit()
+        }
+        but3.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frag_view,ResultFrag()).commit()
+        }
     }
 }
